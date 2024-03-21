9831b5732d564decb44be7b6162c305bc05598f5
Emil Rapp <emilrapp@kth.se>
2024-03-21 21:02:27 +0100

    Update chewbacca.py

--- chewbacca/chewbacca.py
+++ chewbacca/chewbacca.py
@@ -1,5 +1,3 @@
-l = lambda k, a, b: 0 if a == b else 1 + l(k, (a - 1) // k, b) if a > b else 1 + l(k, a, (b - 1) // k)
-_, K, Q = map(int, input().split())
-queries = [tuple(map(lambda x: int(x) - 1, input().split())) for _ in range(Q)]
-results = [str(l(K, *query)) for query in queries]
-print('\n'.join(results))
+l=lambda k,a,b:0if a==b else 1+l(k,(a-1)//k,b)if a>b else 1+l(k,a,(b-1)//k)
+_,k,q=map(int,input().split())
+print('\n'.join(str(l(k,*[int(x)-1 for x in input().split()]))for _ in range(q)))
