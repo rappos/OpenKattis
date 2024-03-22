9cd48b7a545d9084f898fb3d35b9663d057b6aba
Emil Rapp <emilrapp@kth.se>
2024-03-22 08:41:45 +0100

    not done

--- chewbacca/chewbacca2.py
+++ chewbacca/chewbacca2.py
@@ -1,2 +1,7 @@
+import math
+
+def l(k,a,b):
+        return math.ceil(math.log(a, k))
+
 N,K,Q=map(int,input().split())
-print('\n'.join(str(l(K,*[int(x)-1 for x in input().split()]))for _ in range(q)))
+print('\n'.join(str(l(K,*[int(x)-1 for x in input().split()]))for _ in range(Q)))
